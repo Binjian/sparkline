fe715538a171bfb5353f23e5c66ab124f1b22827
binjian xin <binjian.xin@newrizon.com>
2024-04-24 11:19:27 +0800

    [add] 3 sparklines for lower, upper and center
    remove layers

--- speed_control.py
+++ speed_control.py
@@ -32,17 +32,17 @@ class SpeedControl(Static):
     def watch_time(self, time: float) -> None:
         idx_so_far = int(time)
         speed_so_far = df['speed'].to_list()[:idx_so_far]
-        #upper_so_far = df['upper'].to_list()[:idx_so_far]
-        #lower_so_far = df['lower'].to_list()[:idx_so_far]
-        self.query_one(Sparkline).data = speed_so_far
-        #self.query_one(Sparkline, id="upper").data = df['upper'].to_list()
-        #self.query_one(Sparkline, id="lower").data = df['lower'].to_list()
+        lower_so_far = (df['lower']-df['speed']).to_list()[:idx_so_far]
+        upper_so_far = (df['upper']-df['speed']).to_list()[:idx_so_far]
+        self.query_one("#center").data = speed_so_far
+        self.query_one("#lower").data = lower_so_far
+        self.query_one("#upper").data = upper_so_far 
 
     def compose(self) -> ComposeResult:
         # yield Static("Speed Control")
-        #yield Sparkline(df['upper'].to_list(), summary_function=max, id="upper")
-        yield Sparkline(df['speed'].to_list(), summary_function=max, id="centered")
-        #yield Sparkline(df['lower'].to_list(), summary_function=max, id="lower")
+        yield Sparkline(df['upper'].to_list(), summary_function=mean, id="upper")
+        yield Sparkline(df['speed'].to_list(), summary_function=mean, id="center")
+        yield Sparkline(df['lower'].to_list(), summary_function=mean, id="lower")
 
 
 
@@ -50,6 +50,7 @@ class SparklineSummaryFunctionApp(App[None]):
     CSS_PATH = "speed_control.tcss"
 
     def compose(self) -> ComposeResult:
+        yield Static("Speed Control")
         yield SpeedControl()
 
 
